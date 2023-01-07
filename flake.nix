20a10854ccedc570fc9be5ca99ae2c9caacfbed0
Alex <12470950+alex-nt@users.noreply.github.com>
2023-01-07 21:30:09 +0200

    No manual

--- flake.nix
+++ flake.nix
@@ -37,12 +37,12 @@
 
       checks.x86_64-linux.tarball = jobs.tarball;
 
-      htmlDocs = {
-        nixpkgsManual = jobs.manual;
-        nixosManual = (import ./nixos/release-small.nix {
-          nixpkgs = self;
-        }).nixos.manual.x86_64-linux;
-      };
+      # htmlDocs = {
+      #   nixpkgsManual = jobs.manual;
+      #   nixosManual = (import ./nixos/release-small.nix {
+      #     nixpkgs = self;
+      #   }).nixos.manual.x86_64-linux;
+      # };
 
       # The "legacy" in `legacyPackages` doesn't imply that the packages exposed
       # through this attribute are "legacy" packages. Instead, `legacyPackages`
