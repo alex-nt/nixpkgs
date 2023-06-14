06e193eb27d48bd154d9b80c68bfaa5db03ceb35
Alex <12470950+alex-nt@users.noreply.github.com>
2023-06-14 16:01:00 +0300

    maven: 3.8.6 -> 3.9.2

--- pkgs/development/tools/build-managers/apache-maven/default.nix
+++ pkgs/development/tools/build-managers/apache-maven/default.nix
@@ -4,13 +4,13 @@ assert jdk != null;
 
 stdenv.mkDerivation rec {
   pname = "apache-maven";
-  version = "3.8.6";
+  version = "3.9.2";
 
   builder = ./builder.sh;
 
   src = fetchurl {
     url = "mirror://apache/maven/maven-3/${version}/binaries/${pname}-${version}-bin.tar.gz";
-    sha256 = "sha256-xwR6SN62Jqvyb3GrNkPSltubHmfx+qfZiGN96sh2tak=";
+    sha256 = "sha512-900bdeeeae550d2d2b3920fe0e00e41b0069f32c019d566465015bdd1b3866395cbe016e22d95d25d51d3a5e614af2c83ec9b282d73309f644859bbad08b63db";
   };
 
   nativeBuildInputs = [ makeWrapper ];
