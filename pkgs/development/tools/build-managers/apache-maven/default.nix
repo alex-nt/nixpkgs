5ec75781d6589538c78785c6f842b8d0f8e8f158
Alex <12470950+alex-nt@users.noreply.github.com>
2023-06-14 16:20:43 +0300

    apache-maven sri hash

--- pkgs/development/tools/build-managers/apache-maven/default.nix
+++ pkgs/development/tools/build-managers/apache-maven/default.nix
@@ -10,7 +10,7 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "mirror://apache/maven/maven-3/${version}/binaries/${pname}-${version}-bin.tar.gz";
-    sha512 = "sha512-25lvjmxfrpb3w5mph85q44940d0qi5hgpra4rhim1lj1wmy2d3hb3dvygmlzrrki407vbgknajqacyyndnkrkkd2cc2igrbvn8sspdi";
+    sha512 = "sha512-sV2tkd0rvygYE23OPG2z3jOFpbLzrX0AiTPnT+vzu40FRxO+8iBpqBFmovJ9sEQMGiCJEFwQvLXwsW52vcpNiw==";
   };
 
   nativeBuildInputs = [ makeWrapper ];
