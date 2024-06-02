ebc9cba293ad74fb5c98b481d2c4bc28d161554c
Alex <12470950+alex-nt@users.noreply.github.com>
2024-06-02 11:23:37 +0300

    No hash

--- pkgs/development/python-modules/better-exceptions/default.nix
+++ pkgs/development/python-modules/better-exceptions/default.nix
@@ -11,7 +11,7 @@ buildPythonPackage rec {
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-e4e6bc18444d5f04e6e894b10381e5e921d3d544240418162c7db57e9eb3453b=";
+    hash = "";
   };
 
   buildInputs = [];
